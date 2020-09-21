2dacc13110806c972c7f4b03f2354569cbf5555f
Царь и Бог <PlagueEvgeny@yandex.ru>
2020-09-21 13:04:30 +0500

    myonestr

--- 200921/str_2.py
+++ 200921/str_2.py
@@ -1,11 +1,15 @@
 #class_pupils = input("введите имена учеников через запятую: \n")
 
-class_pupils = "Полина, Антон, Арсений, Евгений, Алексей, Тимур"
+class_pupils = "Полина,Антон, Арсений, Евгений, Алексей, Тимур "
 correct_result = ["Полина", "Антон", "Арсений", "Евгений", "Алексей", "Тимур"]
 
 #print("Ученики класса:", class_pupils)
 
-result = class_pupils.split(", ")
+_result = class_pupils.split(",")
+result = []
+for item in _result:
+    #print(item, "->", item.strip())
+    result.append(item.strip())
 
 assert result == correct_result, 'Алгоритм реализован неверно'
 
