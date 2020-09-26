bd4ccb2d7d2b4c618bbaf953335aa2703a6ac6b5
plaguedoctor <PlagueEvgeny@yandex.ru>
2020-09-26 21:12:49 +0500

    home

--- 200921/home_tack1.py
+++ 200921/home_tack1.py
@@ -3,9 +3,8 @@ correct_request_IP_address = '217.168.17.5'
 correct_request_datetime = '17/May/2015:08:05:12'
 
 request_IP_address = row_record.split() [0]
-print(request_IP_address)
 request_datetime = row_record.split() [4]
-print(request_datetime)
+print(request_IP_address, ", ", request_datetime)
 
 assert request_IP_address == correct_request_IP_address
 assert request_datetime == correct_request_datetime
