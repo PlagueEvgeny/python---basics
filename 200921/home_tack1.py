fa231f7a822b145a0849f532e8031d7313b0aae7
plaguedoctor <PlagueEvgeny@yandex.ru>
2020-09-27 21:56:35 +0500

    home

--- 200921/home_tack1.py
+++ 200921/home_tack1.py
@@ -1,12 +1,17 @@
-row_record = '217.168.17.5 - - [ 17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
+row_record = '217.168.17.5 - - [ 17/May/2015:08:05:12 +0000] " GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
 correct_request_IP_address = '217.168.17.5'
 correct_request_datetime = '17/May/2015:08:05:12'
+correct_request_method = 'GET'
+correct_requested_resource = '/downloads/product_2'
 
 request_IP_address = row_record.split() [0]
 request_datetime = row_record.split() [4]
-print(request_IP_address, ", ", request_datetime)
+request_method = row_record.split() [7]
+requested_resource = row_record.split() [8]
+print(request_IP_address, ", ", request_datetime, ", ", request_method, ', ', requested_resource)
 
 assert request_IP_address == correct_request_IP_address
 assert request_datetime == correct_request_datetime
-
+assert request_method == correct_request_method
+assert requested_resource == correct_requested_resource
 
